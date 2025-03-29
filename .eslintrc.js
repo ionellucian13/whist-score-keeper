935527f01d59096da8689439bc1b275d9be69e91
HotelPOS <hotelpos@example.com>
2025-03-29 18:52:00 +0200

    Adăugare configurație ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['react-app', 'react-app/jest'],
+  rules: {
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}; 
